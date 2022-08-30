bdd3ad24d840912de4a755b3ec9d65c5a58ae2dc
khalil sadi <khalilcode01@gmail.com>
2022-08-30 20:56:57 +0300

    Add html , js , css files

---
+++ myScript.js
@@ -0,0 +1,13 @@
+const container = document.getElementsByClassName('container');
+let rows = document.getElementsByClassName('gridRow');
+let cells = document.getElementsByClassName('cell');
+for (r = 0; r < 16; r++) {
+    let row = document.createElement('div');
+    container.appendChild(row).className = 'gridRow'; 
+}
+for (i = 0; i < rows.length; i++) 
+    for (j = 0; j < 16; j++){
+        let newCell = document.createElement('div');
+        rows[j].appendChild(newCell).className = 'cell';
+    }
+

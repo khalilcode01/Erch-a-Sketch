77b6320cf92186ed548d3705de652fe0f8ebf5aa
khalil sadi <khalilcode01@gmail.com>
2022-09-01 16:52:51 +0300

    Rewrite myScript.js

--- myScript.js
+++ myScript.js
@@ -1,15 +1,20 @@
-const container = document.getElementsByClassName('container');
-let rows = document.getElementsByClassName('gridRow');
-let cells = document.getElementsByClassName('cell');
-for (r = 0; r < 16; r++) {
-    let row = document.createElement('div');
-    container.appendChild(row).className = 'gridRow'; 
-}
-for (i = 0; i < rows.length; i++) 
-    for (j = 0; j < 16; j++){
-        let newCell = document.createElement('div');
-        rows[j].appendChild(newCell).className = 'cell';
+let container = document.querySelector('.container');
+function makeGrid(coll, row){
+    for(i = 0; i < coll; i++){
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        container.appendChild(row);
+        for(u = 0; u < coll; u++){
+            let coll = document.createElement('div');
+            coll.setAttribute('class', 'coll');
+            row.appendChild(coll);
+        }
     }
-
-
-    
+}
+makeGrid(4,4);
+let div = document.getElementsByClassName('coll');
+for(let i = 0; i < div.length; i++) {
+  div[i].addEventListener("mouseover", function() {
+    div[i].setAttribute('style', 'background-color: black;')
+  })
+}
